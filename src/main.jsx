966ff8b5f127c25f526883a19b6c646dbe57e689
VotreNomUtilisateur <kevinkouakou113@gmail.com>
2024-02-01 18:19:56 +0000

    removing react router

--- src/main.jsx
+++ src/main.jsx
@@ -2,24 +2,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { Suspense, lazy } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
+import App from "./App.jsx";
 import "./index.css";
-import Loader from "./components/Loader.jsx";
-
-const App = lazy(() => import("./App.jsx"));
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Suspense fallback={<Loader />}>
-      <RouterProvider router={router} />
-    </Suspense>
+    <App />
   </React.StrictMode>
 );
